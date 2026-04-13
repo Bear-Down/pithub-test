4519347c7c2243005d9b61450495fb5f6f41e4ba
Kevin <kevin.dacanay27@gmail.com>
2026-04-12 22:58:33 -0500

    Kevin Dacanay - Committing Frontend

--- pithub/frontend/src/storage.js
+++ pithub/frontend/src/storage.js
@@ -1,5 +1,5 @@
 import { ref, listAll, getDownloadURL } from "firebase/storage";
-import { storage } from "../backend/firebase.js";
+import { storage } from "../../firebase.js"; // Path adjusted for frontend/src/ location
 
 /**
  * Fetches file metadata and download URLs from Firebase Storage.
